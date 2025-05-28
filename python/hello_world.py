e8cdd671b3e40662a8a85bfb6ce4bfff7d4ccc21
Gonzalo <57041998+HarryF6@users.noreply.github.com>
2025-05-28 11:47:21 +0200

    Update hello_world.py

--- python/hello_world.py
+++ python/hello_world.py
@@ -1,14 +1,3 @@
-# Saludo personalizado
-nombre = input("¿Cómo te llamas? ")
-print(f"Hola, {nombre}!")
+# python/hello_world.py
 
-# Pregunta por la edad
-edad = int(input("¿Cuántos años tienes? "))
-
-# Calcula el año en que cumplirá 100
-from datetime import datetime
-año_actual = datetime.now().year
-año_100 = año_actual + (100 - edad)
-
-print(f"{nombre}, cumplirás 100 años en el año {año_100}.")
-
+print("Hello world from script")
