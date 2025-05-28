ca75c0a0903aff82dde3e70991a04bf43a70b393
Gonzalo <57041998+HarryF6@users.noreply.github.com>
2025-05-28 10:38:32 +0200

    Update hello_world.cpp

--- cpp/hello_world.cpp
+++ cpp/hello_world.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World! My name is Gonzalo Ferrando " << std::endl;
-    return 0;
+  std::cout << "Hello, World! My name is Gonzalo Ferrando " << std::endl;
+  return 0;
 }
